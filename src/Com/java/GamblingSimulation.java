3d135a2843c0a23fbbdd5dea865ad95edab10efb
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-19 09:15:29 +0530

    Added: Win or Loose checking conditions

--- src/Com/java/GamblingSimulation.java
+++ src/Com/java/GamblingSimulation.java
@@ -1,11 +1,33 @@
 package Com.java;
 
+import java.util.Random;
+
 public class GamblingSimulation {
-	static final int STAKE_PAY = 100;
-	static final int PER_BET_GAME = 1;
+	public static final int STAKE_PAY = 100;
+	public static final int PER_BET_GAME = 1;
+	public static final int win = 1;
+	public static int stake = 0;
+
+	// Win Check function
+
+	public static void winCheck() {
+		Random random = new Random();
+		int bet = random.nextInt();
+
+		if (bet == win) {
+			stake++; // incrementing
+			System.out.println("Win");
+		} else {
+			stake--; // Decrementing
+			System.out.println("Loss");
+		}
+		System.out.println("Stake = " + stake);
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Gambling Simulation");
+		// Calling win check function
+		winCheck();
 
 	}
 }
