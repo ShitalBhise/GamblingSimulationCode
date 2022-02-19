568be4814e82422f3d7d79cbd36dfd1b930e257d
ShitalBhise <bhiseshital27059@gmail.com>
2022-02-19 08:37:35 +0530

    Added: Stake and bet initialization logic

---
+++ src/Com/java/GamblingSimulation.java
@@ -0,0 +1,11 @@
+package Com.java;
+
+public class GamblingSimulation {
+	static final int STAKE_PAY = 100;
+	static final int PER_BET_GAME = 1;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation");
+
+	}
+}
